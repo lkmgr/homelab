3776f20be30ba5c83e40503714f83acc7c3725e9
lkmgr <7467162+lkmgr@users.noreply.github.com>
2023-08-16 23:45:58 +0200

    fix(pulumi): create known_hosts and private key for docker provider

--- pulumi/nas/provider.ts
+++ pulumi/nas/provider.ts
@@ -1,29 +1,34 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as docker from '@pulumi/docker';
 import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
 
 let provider: docker.Provider | null = null;
 
 const connectDocker = (): docker.Provider => {
   const config = new pulumi.Config();
   const host = config.require('ssh-host');
+  const hostKeys = config.require('ssh-host-keys');
   const privateKey = config.require('ssh-key');
 
-  fs.writeFileSync('/tmp/sshkey', privateKey, {
+  const homedir = os.homedir();
+
+  fs.mkdirSync(path.resolve(homedir, '.ssh'), { mode: 0o700 });
+
+  fs.writeFileSync(path.resolve(homedir, '.ssh/known_hosts'), hostKeys, {
     encoding: 'utf8',
-    mode: 0o600,
+    mode: 0o644,
+  });
+
+  fs.writeFileSync(path.resolve(homedir, '.ssh/id_ed25519'), privateKey, {
+    encoding: 'utf8',
+    mode: 0o400,
   });
 
   const provider = new docker.Provider('nas', {
     host,
-    sshOpts: [
-      '-i',
-      '/tmp/sshkey',
-      '-o',
-      'StrictHostKeyChecking=no',
-      '-o',
-      'UserKnownHostsFile=/dev/null',
-    ],
+    sshOpts: ['-i', path.resolve(homedir, '.ssh/id_ed25519')],
   });
 
   return provider;
