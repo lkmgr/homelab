cc0fb5372893750734e252624c5e1923256bcf1c
lkmgr <7467162+lkmgr@users.noreply.github.com>
2023-08-16 22:31:51 +0200

    feat(pulumi): create docker ssh provider manually

--- pulumi/nas/node-exporter/index.ts
+++ pulumi/nas/node-exporter/index.ts
@@ -1,28 +1,39 @@
 import * as docker from '@pulumi/docker';
 
 import { image } from './config';
+import getProvider from '../provider';
 
-const nodeExporterImage = new docker.RemoteImage('node-exporter', {
-  name: image,
-});
+const provider = getProvider();
 
-const nodeExporterContainer = new docker.Container('node-exporter', {
-  name: 'pulumi-node-exporter',
-  image: nodeExporterImage.imageId,
-  restart: 'unless-stopped',
-  command: [
-    '--path.procfs=/host/proc',
-    '--path.sysfs=/host/sys',
-    '--no-collector.diskstats',
-    '--no-collector.filesystem',
-  ],
-  privileged: true,
-  ports: [{ internal: 9100, external: 9100 }],
-  volumes: [
-    { hostPath: '/proc', containerPath: '/host/proc', readOnly: true },
-    { hostPath: '/sys', containerPath: '/host/sys', readOnly: true },
-  ],
-});
+const nodeExporterImage = new docker.RemoteImage(
+  'node-exporter',
+  {
+    name: image,
+  },
+  { provider },
+);
+
+const nodeExporterContainer = new docker.Container(
+  'node-exporter',
+  {
+    name: 'pulumi-node-exporter',
+    image: nodeExporterImage.imageId,
+    restart: 'unless-stopped',
+    command: [
+      '--path.procfs=/host/proc',
+      '--path.sysfs=/host/sys',
+      '--no-collector.diskstats',
+      '--no-collector.filesystem',
+    ],
+    privileged: true,
+    ports: [{ internal: 9100, external: 9100 }],
+    volumes: [
+      { hostPath: '/proc', containerPath: '/host/proc', readOnly: true },
+      { hostPath: '/sys', containerPath: '/host/sys', readOnly: true },
+    ],
+  },
+  { provider },
+);
 
 export default {
   nodeExporterContainer: nodeExporterContainer.id,
---
+++ pulumi/nas/provider.ts
@@ -0,0 +1,36 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as docker from '@pulumi/docker';
+import * as fs from 'fs';
+
+let provider: docker.Provider | null = null;
+
+const connectDocker = (): docker.Provider => {
+  const config = new pulumi.Config();
+  const host = config.require('ssh-host');
+  const privateKey = config.require('ssh-key');
+
+  fs.writeFileSync('/tmp/sshkey', privateKey, 'utf-8');
+
+  const provider = new docker.Provider('nas', {
+    host,
+    sshOpts: [
+      '-i',
+      '/tmp/sshkey',
+      '-o',
+      'StrictHostKeyChecking=no',
+      '-o',
+      'UserKnownHostsFile=/dev/null',
+    ],
+  });
+
+  return provider;
+};
+
+const getProvider = () => {
+  if (!provider) {
+    provider = connectDocker();
+  }
+  return provider;
+};
+
+export default getProvider;
